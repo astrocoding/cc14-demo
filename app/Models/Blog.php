0239248edf34765b0637b17f2835c393154d8084
astrocoding <zaenalalfian20@gmail.com>
2024-10-15 11:02:30 +0700

    feat: add model fillable & accessor method for image

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'image'];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
 }
